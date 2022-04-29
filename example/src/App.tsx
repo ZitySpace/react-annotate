7fd8ad57a738f9c09e85435e6390c0df41140814
JasonLamv-t <jasonlamv-t@hotmail.com>
2022-04-29 12:28:56 +0800

    mockdata: add segmentations and replace the origin rectangle data with coco dataset.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react'
 
-import { Annotator, RectLabel } from 'react-annotate-image'
+import { Annotator } from 'react-annotate-image'
 import 'react-annotate-image/dist/index.css'
 
-import { pagingData, projectMock } from './mockdata'
-const imagesList = pagingData.map((img) => ({
+// import { data as rect } from './rectangles'
+// import { RectLabel } from 'react-annotate-image'
+// const imagesList = rect.map((img) => ({
+//   ...img,
+//   annotations: img.annotations.map((anno: any, id: number) => {
+//     const { x, y, w, h, category } = anno
+//     return new RectLabel({ x, y, w, h, id, category })
+//   })
+// }))
+
+import { data as segm } from './segmentations'
+import { PolygonLabel } from 'react-annotate-image'
+const imagesList = segm.map((img) => ({
   ...img,
   annotations: img.annotations.map((anno: any, id: number) => {
-    const { x, y, w, h, category } = anno
-    return new RectLabel({ x, y, w, h, id, category })
+    const { points, category } = anno
+    return new PolygonLabel({ points, id, category })
   })
 }))
 
 const App = () => {
-  const colors = projectMock.colors
-  const categoryColors = new Map<string, string>(Object.entries(colors))
-
   return (
     <div className='h-screen w-screen'>
-      <Annotator
-        imagesList={imagesList}
-        index={1}
-        categoryColors={categoryColors}
-      />
+      <Annotator imagesList={imagesList} index={5} />
     </div>
   )
 }
