b34ac54b84301e4cafd329fdcac10e9bd578c935
JasonLamv-t <jasonlamv-t@hotmail.com>
2022-05-27 19:11:40 +0800

    update: use callback in example.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-import { Annotator } from 'react-annotate-image'
-import 'react-annotate-image/dist/index.css'
+import { Annotator } from 'react-annotate-image';
+import 'react-annotate-image/dist/index.css';
 
 // import { data as rect } from './rectangles'
 // import { RectLabel } from 'react-annotate-image'
@@ -13,22 +13,27 @@ import 'react-annotate-image/dist/index.css'
 //   })
 // }))
 
-import { data as segm } from './segmentations'
-import { PolygonLabel } from 'react-annotate-image'
+import { data as segm } from './segmentations';
+import { PolygonLabel } from 'react-annotate-image';
 const imagesList = segm.map((img) => ({
   ...img,
   annotations: img.annotations.map((anno: any, id: number) => {
-    const { points, category } = anno
-    return new PolygonLabel({ points, id, category })
-  })
-}))
+    const { points, category } = anno;
+    return new PolygonLabel({ points, id, category });
+  }),
+}));
 
 const App = () => {
   return (
     <div className='h-screen w-screen'>
-      <Annotator imagesList={imagesList} index={5} />
+      <Annotator
+        imagesList={imagesList}
+        initIndex={6}
+        onSave={console.log}
+        onSwitch={console.log}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
