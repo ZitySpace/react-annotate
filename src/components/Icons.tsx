f0f6248cfe1f336429dd4d5c06663b8c6927f9e9
JasonLamv-t <jasonlamv-t@hotmail.com>
2022-06-23 16:36:57 +0800

    update: add AI icon.

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -281,3 +281,17 @@ export const WarningIcon = Icon(
     <path d='M12 5.99L19.53 19H4.47L12 5.99M12 2L1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z' />
   </svg>
 );
+
+export const AIIcon = Icon(
+  <svg
+    className='h-6 w-6'
+    viewBox='0 0 24 24'
+    strokeWidth='1'
+    stroke='currentColor'
+    fill='currentColor'
+  >
+    <text y='17' x='5'>
+      AI
+    </text>
+  </svg>
+);
