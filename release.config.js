a3ecee0a2ec6e56a043669eb42fe44b8d3f918d9
Rui <rzhengphy@gmail.com>
2022-07-28 23:53:32 +0800

    fix(ci): ignore releasing to npm for now

--- release.config.js
+++ release.config.js
@@ -9,7 +9,7 @@ module.exports = {
         changelogFile: 'CHANGELOG.md',
       },
     ],
-    '@semantic-release/npm',
+    // '@semantic-release/npm',
     '@semantic-release/github',
     [
       '@semantic-release/git',
